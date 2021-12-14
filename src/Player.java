4920a1232a85ad28e34456b51fd2b6dda6f8688b
thewizzid <94404156+thewizzid@users.noreply.github.com>
2021-12-14 20:12:11 +0100

    merge14/12

--- src/Player.java
+++ src/Player.java
@@ -3,6 +3,6 @@ public class Player {
     int cashCount = 50030;
     int foodCount = 0;
     boolean automaticPopulation = true;
-    int[] cropStock = {9,0,0,0,0,0,0,0};
+    int[] cropStock = {0,0,0,0,0,0,0,0};
     //0=Tomato/1=Corn/2=Lettuce/3=Eggplant/4=Potato/5=Peppers/6=Pumpkin/7=Watermelon
 }
